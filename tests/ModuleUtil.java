132bde2e0605e7ddb72cfe16edf772420169bd59
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2017-09-01 20:10:16 +1000

    TRAPBURIAL-15

---
+++ tests/ModuleUtil.java
@@ -0,0 +1,492 @@
+package au.org.intersect.faims.android.util;
+
+import android.view.View;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
+
+public class ModuleUtil extends ModuleHelper {
+    /*
+        Type: tab group
+        Ref:  Login
+     */
+    public static View get_Login(Solo solo) {
+        String ref = "Login";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Mound
+     */
+    public static View get_Mound(Solo solo) {
+        String ref = "Mound";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Start
+     */
+    public static View get_Start(Solo solo) {
+        String ref = "Start";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Welcome
+     */
+    public static View get_Welcome(Solo solo) {
+        String ref = "Welcome";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+    /*
+        Type: tab
+        Ref:  Login/Login
+     */
+    public static View get_Login_Login(Solo solo) {
+        String ref = "Login/Login";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Login/Manual
+     */
+    public static View get_Login_Manual(Solo solo) {
+        String ref = "Login/Manual";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Mound/Condition
+     */
+    public static View get_Mound_Condition(Solo solo) {
+        String ref = "Mound/Condition";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Mound/Mound_Admin
+     */
+    public static View get_Mound_MoundAdmin(Solo solo) {
+        String ref = "Mound/Mound_Admin";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Start/Map
+     */
+    public static View get_Start_Map(Solo solo) {
+        String ref = "Start/Map";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Start/Start
+     */
+    public static View get_Start_Start(Solo solo) {
+        String ref = "Start/Start";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Welcome/Welcome
+     */
+    public static View get_Welcome_Welcome(Solo solo) {
+        String ref = "Welcome/Welcome";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+    /*
+        Type: button
+        Ref:  Login/Login/Log_in
+     */
+    public static View get_Login_Login_Login(Solo solo) {
+        String ref = "Login/Login/Log_in";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Login/Login/Sign_up
+     */
+    public static View get_Login_Login_Signup(Solo solo) {
+        String ref = "Login/Login/Sign_up";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Login/Manual/Go_to_Login_Tab
+     */
+    public static View get_Login_Manual_GotoLoginTab(Solo solo) {
+        String ref = "Login/Manual/Go_to_Login_Tab";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Mound/Condition/Detail_photo_Button_1
+     */
+    public static View get_Mound_Condition_DetailphotoButton1(Solo solo) {
+        String ref = "Mound/Condition/Detail_photo_Button_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Mound/Mound_Admin/Image_Button_1
+     */
+    public static View get_Mound_MoundAdmin_ImageButton1(Solo solo) {
+        String ref = "Mound/Mound_Admin/Image_Button_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Mound/Mound_Admin/Take_From_GPS_1
+     */
+    public static View get_Mound_MoundAdmin_TakeFromGPS1(Solo solo) {
+        String ref = "Mound/Mound_Admin/Take_From_GPS_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Mound/Mound_Admin/Validate
+     */
+    public static View get_Mound_MoundAdmin_Validate(Solo solo) {
+        String ref = "Mound/Mound_Admin/Validate";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Start/Map/Center_Me_1
+     */
+    public static View get_Start_Map_CenterMe1(Solo solo) {
+        String ref = "Start/Map/Center_Me_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Start/Search/Search_Button
+     */
+    public static View get_Start_Search_SearchButton(Solo solo) {
+        String ref = "Start/Search/Search_Button";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Start/Start/Record_New_Mound
+     */
+    public static View get_Start_Start_RecordNewMound(Solo solo) {
+        String ref = "Start/Start/Record_New_Mound";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Welcome/Welcome/Start
+     */
+    public static View get_Welcome_Welcome_Start(Solo solo) {
+        String ref = "Welcome/Welcome/Start";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: camera
+        Ref:  Mound/Condition/Detail_photo
+     */
+    public static View get_Mound_Condition_Detailphoto(Solo solo) {
+        String ref = "Mound/Condition/Detail_photo";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: camera
+        Ref:  Mound/Mound_Admin/Image
+     */
+    public static View get_Mound_MoundAdmin_Image(Solo solo) {
+        String ref = "Mound/Mound_Admin/Image";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: checkbox
+        Ref:  Mound/Condition/Associated_material
+     */
+    public static View get_Mound_Condition_Associatedmaterial(Solo solo) {
+        String ref = "Mound/Condition/Associated_material";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: checkbox
+        Ref:  Mound/Condition/Robbers_trenches
+     */
+    public static View get_Mound_Condition_Robberstrenches(Solo solo) {
+        String ref = "Mound/Condition/Robbers_trenches";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Login/Login/Please_select_a_user
+     */
+    public static View get_Login_Login_Pleaseselectauser(Solo solo) {
+        String ref = "Login/Login/Please_select_a_user";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Mound/Condition/Condition
+     */
+    public static View get_Mound_Condition_Condition(Solo solo) {
+        String ref = "Mound/Condition/Condition";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Mound/Condition/Source_of_Impact
+     */
+    public static View get_Mound_Condition_SourceofImpact(Solo solo) {
+        String ref = "Mound/Condition/Source_of_Impact";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Condition/Associated_features
+     */
+    public static EditText get_Mound_Condition_Associatedfeatures(Solo solo) {
+        String ref = "Mound/Condition/Associated_features";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Condition/Notes_on_Condition
+     */
+    public static EditText get_Mound_Condition_NotesonCondition(Solo solo) {
+        String ref = "Mound/Condition/Notes_on_Condition";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Condition/Notes_on_shape_and_dimension
+     */
+    public static EditText get_Mound_Condition_Notesonshapeanddimension(Solo solo) {
+        String ref = "Mound/Condition/Notes_on_shape_and_dimension";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Accuracy
+     */
+    public static View get_Mound_MoundAdmin_Accuracy(Solo solo) {
+        String ref = "Mound/Mound_Admin/Accuracy";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Description_of_mound_or_location
+     */
+    public static EditText get_Mound_MoundAdmin_Descriptionofmoundorlocation(Solo solo) {
+        String ref = "Mound/Mound_Admin/Description_of_mound_or_location";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Diameter_Max
+     */
+    public static EditText get_Mound_MoundAdmin_DiameterMax(Solo solo) {
+        String ref = "Mound/Mound_Admin/Diameter_Max";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Diameter_Min
+     */
+    public static EditText get_Mound_MoundAdmin_DiameterMin(Solo solo) {
+        String ref = "Mound/Mound_Admin/Diameter_Min";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Easting
+     */
+    public static View get_Mound_MoundAdmin_Easting(Solo solo) {
+        String ref = "Mound/Mound_Admin/Easting";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Height_Max
+     */
+    public static EditText get_Mound_MoundAdmin_HeightMax(Solo solo) {
+        String ref = "Mound/Mound_Admin/Height_Max";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Height_Min
+     */
+    public static EditText get_Mound_MoundAdmin_HeightMin(Solo solo) {
+        String ref = "Mound/Mound_Admin/Height_Min";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Latitude
+     */
+    public static View get_Mound_MoundAdmin_Latitude(Solo solo) {
+        String ref = "Mound/Mound_Admin/Latitude";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Longitude
+     */
+    public static View get_Mound_MoundAdmin_Longitude(Solo solo) {
+        String ref = "Mound/Mound_Admin/Longitude";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Mound_ID
+     */
+    public static View get_Mound_MoundAdmin_MoundID(Solo solo) {
+        String ref = "Mound/Mound_Admin/Mound_ID";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Mound_Name
+     */
+    public static EditText get_Mound_MoundAdmin_MoundName(Solo solo) {
+        String ref = "Mound/Mound_Admin/Mound_Name";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Mound/Mound_Admin/Northing
+     */
+    public static View get_Mound_MoundAdmin_Northing(Solo solo) {
+        String ref = "Mound/Mound_Admin/Northing";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Start/Search/Search_Term
+     */
+    public static EditText get_Start_Search_SearchTerm(Solo solo) {
+        String ref = "Start/Search/Search_Term";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Start/Start/Next_Mound_ID
+     */
+    public static EditText get_Start_Start_NextMoundID(Solo solo) {
+        String ref = "Start/Start/Next_Mound_ID";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: list
+        Ref:  Start/Search/Entity_List
+     */
+    public static View get_Start_Search_EntityList(Solo solo) {
+        String ref = "Start/Search/Entity_List";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: list
+        Ref:  Start/Start/My_Last_3_Records
+     */
+    public static View get_Start_Start_MyLast3Records(Solo solo) {
+        String ref = "Start/Start/My_Last_3_Records";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: map
+        Ref:  Start/Map/Map
+     */
+    public static View get_Start_Map_Map(Solo solo) {
+        String ref = "Start/Map/Map";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: picture
+        Ref:  Mound/Condition/Landuse_around_Mound
+     */
+    public static View get_Mound_Condition_LandusearoundMound(Solo solo) {
+        String ref = "Mound/Condition/Landuse_around_Mound";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: picture
+        Ref:  Mound/Condition/Landuse_on_top_of_Mound
+     */
+    public static View get_Mound_Condition_LanduseontopofMound(Solo solo) {
+        String ref = "Mound/Condition/Landuse_on_top_of_Mound";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: webview
+        Ref:  Login/Login/Signup_Guide_1
+     */
+    public static View get_Login_Login_SignupGuide1(Solo solo) {
+        String ref = "Login/Login/Signup_Guide_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: webview
+        Ref:  Login/Manual/Manual
+     */
+    public static View get_Login_Manual_Manual(Solo solo) {
+        String ref = "Login/Manual/Manual";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: webview
+        Ref:  Welcome/Welcome/Logo
+     */
+    public static View get_Welcome_Welcome_Logo(Solo solo) {
+        String ref = "Welcome/Welcome/Logo";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+}
